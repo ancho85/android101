a2ed6256191e3277d0f4f5f6ca79fa8ba6333696
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2014-10-11 11:29:21 -0300

    identacion y tarea2

--- workspace/Calculadora/src/com/example/calculadora/MainActivity.java
+++ workspace/Calculadora/src/com/example/calculadora/MainActivity.java
@@ -10,43 +10,43 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	int result;
-	int valor1;
-	int valor2;
-	EditText n1;
-	EditText n2;
-	Button calcularButton;
-	TextView resultView;
-	
+    int result;
+    int valor1;
+    int valor2;
+    EditText n1;
+    EditText n2;
+    Button calcularButton;
+    TextView resultView;
+    
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	
-	// se hace un cast de los controles que tenemos en el layout
-	n1 = (EditText) findViewById(R.id.editTextValor1); // R.id contiene todas la referencias a los xml
-	n2 = (EditText) findViewById(R.id.editTextValor2);
-	calcularButton = (Button) findViewById(R.id.buttonCalcular1);
-	resultView = (TextView) findViewById(R.id.textViewResultado1);
-	
-	calcularButton.setOnClickListener(new OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			Toast.makeText(MainActivity.this, Integer.toString(calcular()), Toast.LENGTH_LONG).show(); // esta es una notificacion
-			resultView.setText(Integer.toString(calcular())); //reemplaza el texto en el edittext
-			
-		}
-		
-		private int calcular(){
-			valor1 = Integer.parseInt(n1.getText().toString());
-			valor2 = Integer.parseInt(n2.getText().toString());
-			result = valor1 * valor2;
-			return result;
-		}
-	});
-	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    
+    // se hace un cast de los controles que tenemos en el layout
+    n1 = (EditText) findViewById(R.id.editTextValor1); // R.id contiene todas la referencias a los xml
+    n2 = (EditText) findViewById(R.id.editTextValor2);
+    calcularButton = (Button) findViewById(R.id.buttonCalcular1);
+    resultView = (TextView) findViewById(R.id.textViewResultado1);
+    
+    calcularButton.setOnClickListener(new OnClickListener() {
+        
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, Integer.toString(calcular()), Toast.LENGTH_LONG).show(); // esta es una notificacion
+            resultView.setText(Integer.toString(calcular())); //reemplaza el texto en el edittext
+            
+        }
+        
+        private int calcular(){
+            valor1 = Integer.parseInt(n1.getText().toString());
+            valor2 = Integer.parseInt(n2.getText().toString());
+            result = valor1 * valor2;
+            return result;
+        }
+    });
+    
 
-	}
+    }
 }
