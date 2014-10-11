0304fb898d20e9a5f60bbd5ce4258a11226dcb01
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2014-10-11 18:49:40 -0300

    ajustes de identación (tabs por espacios)

--- workspace/Calculadora/src/com/example/calculadora/MainActivity.java
+++ workspace/Calculadora/src/com/example/calculadora/MainActivity.java
@@ -25,44 +25,48 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // se hace un cast de los controles que tenemos en el layout
-    n1 = (EditText) findViewById(R.id.editTextValor1); // R.id contiene todas la referencias a los xml
-    n2 = (EditText) findViewById(R.id.editTextValor2);
-    calcularButton = (Button) findViewById(R.id.buttonCalcular1);
-    resultView = (TextView) findViewById(R.id.textViewResultado1);
+        // se hace un cast de los controles que tenemos en el layout
+        n1 = (EditText) findViewById(R.id.editTextValor1); // R.id contiene
+                                                           // todas la
+                                                           // referencias a los
+                                                           // xml
+        n2 = (EditText) findViewById(R.id.editTextValor2);
+        calcularButton = (Button) findViewById(R.id.buttonCalcular1);
+        resultView = (TextView) findViewById(R.id.textViewResultado1);
 
-    calcularButton.setOnClickListener(new OnClickListener() {
-        
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(MainActivity.this, Integer.toString(calcular()), Toast.LENGTH_LONG).show(); // esta es una notificacion
-            resultView.setText(Integer.toString(calcular())); //reemplaza el texto en el edittext
-            
-        }
-        
-        private int calcular(){
-            valor1 = Integer.parseInt(n1.getText().toString());
-            valor2 = Integer.parseInt(n2.getText().toString());
-            result = valor1 * valor2;
-            return result;
-        }
-    });
-    
-    /* otraActividad = (Button) findViewById(R.id.buttonOtraActivity);
-    otraActividad.setOnClickListener(new OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			startActivity(new Intent(this, OtraActivity.class));
-		}
-	});
-	*/
-    
+        calcularButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, Integer.toString(calcular()),
+                        Toast.LENGTH_LONG).show(); // esta es una notificacion
+                resultView.setText(Integer.toString(calcular())); // reemplaza
+                                                                  // el texto en
+                                                                  // el edittext
+
+            }
+
+            private int calcular() {
+                valor1 = Integer.parseInt(n1.getText().toString());
+                valor2 = Integer.parseInt(n2.getText().toString());
+                result = valor1 * valor2;
+                return result;
+            }
+        });
+
+        /*
+         * otraActividad = (Button) findViewById(R.id.buttonOtraActivity);
+         * otraActividad.setOnClickListener(new OnClickListener() {
+         * 
+         * @Override public void onClick(View v) { startActivity(new
+         * Intent(this, OtraActivity.class)); } });
+         */
+
     }
 
-    public void onClickAction(View view){
+    public void onClickAction(View view) {
         Intent otra = new Intent(this, OtraActivity.class);
-        otra.putExtra("valor","hola");
-          startActivity(otra);
-     };
+        otra.putExtra("valor", "hola");
+        startActivity(otra);
+    };
 }
--- workspace/Calculadora/src/com/example/calculadora/OtraActivity.java
+++ workspace/Calculadora/src/com/example/calculadora/OtraActivity.java
@@ -7,17 +7,17 @@ import android.view.View;
 import android.widget.TextView;
 
 public class OtraActivity extends Activity {
-	@Override
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.otra_activity_main);
-		Intent intent = getIntent();
-		TextView tw = (TextView) findViewById(R.id.tw_otra);
-		tw.setText(intent.getStringExtra("valor"));
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otra_activity_main);
+        Intent intent = getIntent();
+        TextView tw = (TextView) findViewById(R.id.tw_otra);
+        tw.setText(intent.getStringExtra("valor"));
+    }
 
-	public void volverMain(View view){
-		finish();
-	}
+    public void volverMain(View view) {
+        finish();
+    }
 
 }
