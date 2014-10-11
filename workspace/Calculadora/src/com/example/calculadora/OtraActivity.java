47f917792db8a2fca87afad75f8ed36652154a88
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2014-10-11 16:31:11 -0300

    OtraActivity botón volver para finalizar el activity de intent. Obtención del valor pasado por el intent padre. Transpaso de valores

--- workspace/Calculadora/src/com/example/calculadora/OtraActivity.java
+++ workspace/Calculadora/src/com/example/calculadora/OtraActivity.java
@@ -1,13 +1,23 @@
 package com.example.calculadora;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class OtraActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.otra_activity_main);
+		Intent intent = getIntent();
+		TextView tw = (TextView) findViewById(R.id.tw_otra);
+		tw.setText(intent.getStringExtra("valor"));
+	}
+
+	public void volverMain(View view){
+		finish();
 	}
 
 }
