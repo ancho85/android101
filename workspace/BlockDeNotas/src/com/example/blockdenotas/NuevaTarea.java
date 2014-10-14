c972f939049cae6cbd47a615f4b8e85a4c4cf02d
Carlos Gomez <ancho.corp@gmail.com>
2014-10-13 23:47:03 -0300

    campos de edicion y radiobuttons

--- workspace/BlockDeNotas/src/com/example/blockdenotas/NuevaTarea.java
+++ workspace/BlockDeNotas/src/com/example/blockdenotas/NuevaTarea.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class NuevaTarea extends Activity {
     @Override
@@ -12,12 +15,27 @@ public class NuevaTarea extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
         Intent intent = getIntent();
-        TextView tw = (TextView) findViewById(R.id.tw_otra);
-        tw.setText(intent.getStringExtra("valor"));
+        // TextView tw = (TextView) findViewById(R.id.tv_header);
+        // tw.setText(intent.getStringExtra("valor"));
     }
 
-    public void volverMain(View view) {
+    public void guardar(View view) {
+        Toast.makeText(NuevaTarea.this, "Nota guardada", Toast.LENGTH_LONG).show();
         finish();
+    }
+
+    public void changePriorityColor(View view){
+        EditText et_nota = (EditText) findViewById(R.id.et_nota);
+    	final RadioGroup rg = (RadioGroup) findViewById(R.id.rg_priority);        
+        int id_rb_checket = rg.getCheckedRadioButtonId();
+        
+        switch (id_rb_checket){
+	         case R.id.rb_high:
+	        	 break;
+	         case R.id.rb_medium:
+	        	 break;
+	         case R.id.rb_low:
+	        	 break;
     }
 
 }
