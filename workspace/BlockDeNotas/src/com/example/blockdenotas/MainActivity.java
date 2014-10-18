4a30e5ffab553394a36fde6fee476202afb68e4a
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2014-10-18 15:39:39 -0300

    ArrayAdapter para Titulares

--- workspace/BlockDeNotas/src/com/example/blockdenotas/MainActivity.java
+++ workspace/BlockDeNotas/src/com/example/blockdenotas/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.blockdenotas;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -17,12 +21,31 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         datos = new Titular[] {
-                new Titular("Título 1", "Subtítulo largo 1", 3),
-                new Titular("Título 2", "Subtítulo largo 2", 1),
+                new Titular("Título 1", "Subtítulo largo 1", 3), 
+                new Titular("Título 2", "Subtítulo largo 2", 1), 
                 new Titular("Título 3", "Subtítulo largo 3", 2),
-                new Titular("Título 4", "Subtítulo largo 4", 1),
-                new Titular("Título 5", "Subtítulo largo 5", 3) 
-                };
+                new Titular("Título 4", "Subtítulo largo 4", 1), 
+                new Titular("Título 5", "Subtítulo largo 5", 3)
+        };
+
+        class AdaptadorTitulares extends ArrayAdapter {
+            Activity context;
+
+            AdaptadorTitulares(Activity context) {
+                super(context, R.layout.listitem_titular, datos); // se hace referencia al nuevo layout xml para el ListView
+                this.context = context;
+            }
+
+            public View getView(int position, View convertView, ViewGroup parent) {
+                LayoutInflater inflater = context.getLayoutInflater();
+                View item = inflater.inflate(R.layout.listitem_titular, null);
+                TextView lblTitulo = (TextView) item.findViewById(R.id.LblTitulo);
+                lblTitulo.setText(datos[position].getTitulo());
+                TextView lblSubtitulo = (TextView) item.findViewById(R.id.LblSubTitulo);
+                lblSubtitulo.setText(datos[position].getSubtitulo());
+                return (item);
+            }
+        }
     }
 
     @Override
