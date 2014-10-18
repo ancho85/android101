bed41634ab062b5049bf11d68cfb0a3ec673dee8
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2014-10-18 09:21:18 -0300

    xml, creacion de Spinner (combobox) creacion de un adaptador de tipo ArrayAdapter definicion del dropdown resource del adaptador establecimiento del adaptador del combobox definición de datos del array

--- workspace/pruebalistas/src/com/example/pruebalistas/MainActivity.java
+++ workspace/pruebalistas/src/com/example/pruebalistas/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +13,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final String[] datos = new String[]{"Elem1","Elem2","Elem3","Elem4","Elem5"}; 
+        ArrayAdapter <String> adaptador = 
+                new ArrayAdapter <String> (this, android.R.layout.simple_spinner_item, datos);
+
+        final Spinner cmbOpciones = (Spinner)findViewById(R.id.CmbOpciones);
+
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    
+        cmbOpciones.setAdapter(adaptador);
     }
 
     @Override
