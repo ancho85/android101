5cf0f61fa2f183f67c80320f6bd8d0afd180ae07
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2014-10-22 15:50:43 -0300

    prueba de conexión SQLite

---
+++ workspace/SQLiteTest/src/com/example/sqlitetest/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.sqlitetest;
+
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Abrimos la base de datos 'DBUsuarios' en modo escritura
+        UsuariosSQLiteHelper usdbh = new UsuariosSQLiteHelper(this, "DBUsuarios", null, 1);
+
+        SQLiteDatabase db = usdbh.getWritableDatabase();
+
+        // Si hemos abierto correctamente la base de datos
+        if (db != null) {
+            // Insertamos 5 usuarios de ejemplo
+            for (int i = 1; i <= 5; i++) {
+                // Generamos los datos
+                int codigo = i;
+                String nombre = "Usuario" + i;
+
+                // Insertamos los datos en la tabla Usuarios
+                db.execSQL("INSERT INTO Usuarios (codigo, nombre) " + "VALUES (" + codigo + ", '" + nombre + "')");
+            }
+
+            // Cerramos la base de datos
+            db.close();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ workspace/SQLiteTest/src/com/example/sqlitetest/UsuariosSQLiteHelper.java
@@ -0,0 +1,46 @@
+package com.example.sqlitetest;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UsuariosSQLiteHelper extends SQLiteOpenHelper {
+
+    // Sentencia SQL para crear la tabla de Usuarios
+    String sqlCreate = "CREATE TABLE Usuarios (codigo INTEGER, nombre TEXT)";
+
+    public UsuariosSQLiteHelper(Context context, String name, CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        // TODO Auto-generated constructor stub
+    }
+
+    public UsuariosSQLiteHelper(Context context, String name, CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Se ejecuta la sentencia SQL de creación de la tabla
+        db.execSQL(sqlCreate);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // NOTA: Por simplicidad del ejemplo aquí utilizamos directamente la opción de
+        // eliminar la tabla anterior y crearla de nuevo vacía con el nuevo formato.
+        // Sin embargo lo normal será que haya que migrar datos de la tabla antigua
+        // a la nueva, por lo que este método debería ser más elaborado.
+
+        // Se elimina la versión anterior de la tabla
+        db.execSQL("DROP TABLE IF EXISTS Usuarios");
+
+        // Se crea la nueva versión de la tabla
+        db.execSQL(sqlCreate);
+
+    }
+
+}
