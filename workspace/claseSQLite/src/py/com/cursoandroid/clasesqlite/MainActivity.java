5d98f665c841d5ab6ed6c97ed955f619977e9e9c
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2014-10-25 08:55:23 -0300

    ajustes de identación

--- workspace/claseSQLite/src/py/com/cursoandroid/clasesqlite/MainActivity.java
+++ workspace/claseSQLite/src/py/com/cursoandroid/clasesqlite/MainActivity.java
@@ -1,4 +1,5 @@
 package py.com.cursoandroid.clasesqlite;
+
 import android.app.Activity;
 import android.content.ContentValues;
 import android.database.Cursor;
@@ -11,198 +12,163 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	
-	private Button btnInsertar;
-	private Button btnActualizar;
-	private Button btnEliminar;
-	private Button btnListar;
-	
-	private EditText txtNombre;
-	private EditText txtEmail;
-	
-	private ListView lstUsuarios;
-	SQLiteDatabase db;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		 
-        //Abrimos la base de datos 'DBUsuarios' en modo escritura
-        UsuariosSQLiteHelper usdbh =
-            new UsuariosSQLiteHelper(this, "DBUsuarios2", null, 1);
- 
+    private Button btnInsertar;
+    private Button btnActualizar;
+    private Button btnEliminar;
+    private Button btnListar;
+
+    private EditText txtNombre;
+    private EditText txtEmail;
+
+    private ListView lstUsuarios;
+    SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Abrimos la base de datos 'DBUsuarios' en modo escritura
+        UsuariosSQLiteHelper usdbh = new UsuariosSQLiteHelper(this, "DBUsuarios2", null, 1);
+
         db = usdbh.getWritableDatabase();
         CrearEventosOnClick();
- 
-        //Si hemos abierto correctamente la base de datos
-		        if(db != null)
-		        {
-		            //Insertamos 5 usuarios de ejemplo
-		            /*for(int i=1; i<=5; i++)
-		            {
-		                //Generamos los datos
-		                int codigo = i;
-		                String nombre = "Usuario" + i;
-		                String email  = "usuario" + i + "@gmail.com";
-		 
-		                //Insertamos los datos en la tabla Usuarios
-		                db.execSQL("INSERT INTO Usuarios (nombre, email) " +
-		                           "VALUES ('" + nombre +"', '"+email+"')");
-		            }*/
-		 
-		            //Cerramos la base de datos
-		           // db.close();
-		        }
 
-			
-		
-	}
-	
-	private void CrearEventosOnClick()
-	{
-		  
-        btnInsertar = (Button)findViewById(R.id.Insertar);
-        btnActualizar = (Button)findViewById(R.id.Actualizar);
-        btnEliminar = (Button)findViewById(R.id.Borrar);
-        btnListar = (Button)findViewById(R.id.Listar);
-        
-        txtNombre = (EditText)findViewById(R.id.TxtNombre);
-        txtEmail = (EditText)findViewById(R.id.TxtEmail);
-        
-        lstUsuarios =  (ListView)findViewById(R.id.LstValores);
-        
-        
-        btnInsertar.setOnClickListener(new OnClickListener() 
-        {
-			@Override
-			public void onClick(View v) {
-				insertarDatos(txtNombre.getText().toString(),txtEmail.getText().toString());
-			}
-		});
-        
-        btnEliminar.setOnClickListener(new OnClickListener() 
-        {
-			@Override
-			public void onClick(View v) {
-				String[] nombre = new String[1];
-				nombre[0] = txtNombre.getText().toString();
-				eliminarDatos (nombre);
-				
-			}
-		});
-        btnListar.setOnClickListener(new OnClickListener() 
-        {
-			@Override
-			public void onClick(View v) {
-				lstUsuarios.setAdapter(listarDatos());
-			}
-		});
-        btnActualizar.setOnClickListener(new OnClickListener() 
-        {
-			@Override
-			public void onClick(View v) {
-				String[] nombre = new String[1];
-				nombre[0] = txtNombre.getText().toString();
-				actualizarDatos(nombre,txtEmail.getText().toString());
-				
-			}
-		});
-	
-	}
+        // Si hemos abierto correctamente la base de datos
+        if (db != null) {
+            // Insertamos 5 usuarios de ejemplo
+            /*
+             * for(int i=1; i<=5; i++) { //Generamos los datos int codigo = i; String nombre = "Usuario" + i; String email = "usuario" + i + "@gmail.com"; //Insertamos los datos en la tabla Usuarios
+             * db.execSQL("INSERT INTO Usuarios (nombre, email) " + "VALUES ('" + nombre +"', '"+email+"')"); }
+             */
 
-	
-	
-
-	
-/*-----------------------------METODOS DE BASE DE DATOS-------------------------------------------------*/
-	private void insertarDatos (String nombre, String email){
-		//Creamos el registro a insertar como objeto ContentValues
-		ContentValues nuevoRegistro = new ContentValues();
-		nuevoRegistro.put("nombre", nombre);
-		nuevoRegistro.put("email",email);
-		 
-		//Insertamos el registro en la base de datos
-		try
-		{
-			db.insert("Usuarios", null, nuevoRegistro);
-		}
-		catch(Exception ex)
-        {
-        	Log.e("SQLite","Error!", ex);
+            // Cerramos la base de datos
+            // db.close();
         }
-	}
-	private void actualizarDatos (String[] ArgNombres, String email){
-	
-		//OJO: la funcion esta preparada para recibir un solo nombre como argumento.
-		//Actualizar dos registros con update(), utilizando argumentos
-		ContentValues valores = new ContentValues();
-		valores.put("email",email);
-		 
-		String[] args = new String[1];
-		args = ArgNombres;
-		try{
-			db.update("Usuarios", valores, "nombre=?", args);
-		}
-		catch(Exception ex)
-        {
-        	Log.e("SQLite","Error!", ex);
+
+    }
+
+    private void CrearEventosOnClick() {
+
+        btnInsertar = (Button) findViewById(R.id.Insertar);
+        btnActualizar = (Button) findViewById(R.id.Actualizar);
+        btnEliminar = (Button) findViewById(R.id.Borrar);
+        btnListar = (Button) findViewById(R.id.Listar);
+
+        txtNombre = (EditText) findViewById(R.id.TxtNombre);
+        txtEmail = (EditText) findViewById(R.id.TxtEmail);
+
+        lstUsuarios = (ListView) findViewById(R.id.LstValores);
+
+        btnInsertar.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertarDatos(txtNombre.getText().toString(), txtEmail.getText().toString());
+            }
+        });
+
+        btnEliminar.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] nombre = new String[1];
+                nombre[0] = txtNombre.getText().toString();
+                eliminarDatos(nombre);
+
+            }
+        });
+        btnListar.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lstUsuarios.setAdapter(listarDatos());
+            }
+        });
+        btnActualizar.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] nombre = new String[1];
+                nombre[0] = txtNombre.getText().toString();
+                actualizarDatos(nombre, txtEmail.getText().toString());
+
+            }
+        });
+
+    }
+
+    /*-----------------------------METODOS DE BASE DE DATOS-------------------------------------------------*/
+    private void insertarDatos(String nombre, String email) {
+        // Creamos el registro a insertar como objeto ContentValues
+        ContentValues nuevoRegistro = new ContentValues();
+        nuevoRegistro.put("nombre", nombre);
+        nuevoRegistro.put("email", email);
+
+        // Insertamos el registro en la base de datos
+        try {
+            db.insert("Usuarios", null, nuevoRegistro);
         }
-		
-	}
-
-
-	private void eliminarDatos (String[] ArgNombres){
-						 
-		String[] args = new String[1];
-		args = ArgNombres;
-		try{
-			db.delete("Usuarios", "nombre=?", args);
-		}
-		catch(Exception ex)
-        {
-        	Log.e("SQLite","Error!", ex);
+        catch (Exception ex) {
+            Log.e("SQLite", "Error!", ex);
         }
-	}
-	
-	
-	private ArrayAdapter<String> listarDatos()
-	{
-		
-		String[] campos = new String[] {"nombre", "email"};
-		String[] Usuarios;
-		//String[] args = new String[] {"usu1"}; --si necesitamos filtrar por algun valor aqui deberian ir los valores
-		 
-		Cursor c = db.query("Usuarios", campos,null, null, null, null, null);
-		
-		Usuarios = new String[c.getCount()];
-		//Nos aseguramos de que existe al menos un registro
-		if (c.moveToFirst()) {
-		     //Recorremos el cursor hasta que no haya más registros
-			int i =0;
-		     do {
-		          String usuario = c.getString(0);
-		          String email = c.getString(1);
-		          Usuarios[i] = "" + usuario + " | " + email;
-		          i++;
-		          
-		     } while(c.moveToNext());
-		}
+    }
 
-		ArrayAdapter<String> adaptador =
-    		    new ArrayAdapter<String>(MainActivity.this,
-    		        android.R.layout.simple_list_item_1, Usuarios);
-		return adaptador;
-	
-	}
+    private void actualizarDatos(String[] ArgNombres, String email) {
+
+        // OJO: la funcion esta preparada para recibir un solo nombre como argumento.
+        // Actualizar dos registros con update(), utilizando argumentos
+        ContentValues valores = new ContentValues();
+        valores.put("email", email);
+
+        String[] args = new String[1];
+        args = ArgNombres;
+        try {
+            db.update("Usuarios", valores, "nombre=?", args);
+        }
+        catch (Exception ex) {
+            Log.e("SQLite", "Error!", ex);
+        }
+
+    }
+
+    private void eliminarDatos(String[] ArgNombres) {
+
+        String[] args = new String[1];
+        args = ArgNombres;
+        try {
+            db.delete("Usuarios", "nombre=?", args);
+        }
+        catch (Exception ex) {
+            Log.e("SQLite", "Error!", ex);
+        }
+    }
+
+    private ArrayAdapter<String> listarDatos() {
+
+        String[] campos = new String[] { "nombre", "email" };
+        String[] Usuarios;
+        // String[] args = new String[] {"usu1"}; --si necesitamos filtrar por algun valor aqui deberian ir los valores
+
+        Cursor c = db.query("Usuarios", campos, null, null, null, null, null);
+
+        Usuarios = new String[c.getCount()];
+        // Nos aseguramos de que existe al menos un registro
+        if (c.moveToFirst()) {
+            // Recorremos el cursor hasta que no haya más registros
+            int i = 0;
+            do {
+                String usuario = c.getString(0);
+                String email = c.getString(1);
+                Usuarios[i] = "" + usuario + " | " + email;
+                i++;
+
+            }
+            while (c.moveToNext());
+        }
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, Usuarios);
+        return adaptador;
+
+    }
 
 }
-
-
-
-
-
-
--- workspace/claseSQLite/src/py/com/cursoandroid/clasesqlite/UsuariosSQLiteHelper.java
+++ workspace/claseSQLite/src/py/com/cursoandroid/clasesqlite/UsuariosSQLiteHelper.java
@@ -1,45 +1,42 @@
 package py.com.cursoandroid.clasesqlite;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
- 
+
 public class UsuariosSQLiteHelper extends SQLiteOpenHelper {
- 
-    //Sentencia SQL para crear la tabla de Usuarios
+
+    // Sentencia SQL para crear la tabla de Usuarios
     String sqlCreate = "CREATE TABLE Usuarios (nombre TEXT, email TEXT)";
- 
-    public UsuariosSQLiteHelper(Context contexto, String nombre,
-                               CursorFactory factory, int version) {
+
+    public UsuariosSQLiteHelper(Context contexto, String nombre, CursorFactory factory, int version) {
         super(contexto, nombre, factory, version);
     }
- 
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        //Se ejecuta la sentencia SQL de creación de la tabla
-    	try{
-    		db.execSQL(sqlCreate);
-    	}
-    	catch(Exception ex)
-        {
-        	Log.e("SQLite","Error!", ex);
+        // Se ejecuta la sentencia SQL de creación de la tabla
+        try {
+            db.execSQL(sqlCreate);
+        }
+        catch (Exception ex) {
+            Log.e("SQLite", "Error!", ex);
         }
     }
- 
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int versionAnterior, int versionNueva) {
-        //NOTA: Por simplicidad del ejemplo aquí utilizamos directamente la opción de
-        //      eliminar la tabla anterior y crearla de nuevo vacía con el nuevo formato.
-        //      Sin embargo lo normal será que haya que migrar datos de la tabla antigua
-        //      a la nueva, por lo que este método debería ser más elaborado.
- 
-        //Se elimina la versión anterior de la tabla
+        // NOTA: Por simplicidad del ejemplo aquí utilizamos directamente la opción de
+        // eliminar la tabla anterior y crearla de nuevo vacía con el nuevo formato.
+        // Sin embargo lo normal será que haya que migrar datos de la tabla antigua
+        // a la nueva, por lo que este método debería ser más elaborado.
+
+        // Se elimina la versión anterior de la tabla
         db.execSQL("DROP TABLE IF EXISTS Usuarios");
- 
-        //Se crea la nueva versión de la tabla
+
+        // Se crea la nueva versión de la tabla
         db.execSQL(sqlCreate);
     }
 }
-
-
